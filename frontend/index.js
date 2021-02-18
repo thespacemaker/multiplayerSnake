7d3c68bca57371f5360a6a0f89bd6f559a0da701
Adnan Chahbandar <31951968+thespacemaker@users.noreply.github.com>
2021-02-17 19:09:06 -0600

    updated socket endpoint to production

--- frontend/index.js
+++ frontend/index.js
@@ -2,7 +2,7 @@ const BG_COLOUR = '#231f20';
 const SNAKE_COLOUR = '#c2c2c2';
 const FOOD_COLOUR = '#e66916';
 
-const socket = io('http://localhost:3000');
+const socket = io('http://3.139.87.87');
 
 socket.on('init', handleInit);
 socket.on('gameState', handleGameState);
